9a07e44a62f91a786d51ba596e3912ca00e7bb37
JerryTan <jryz101@gmail.com>
2019-02-23 22:48:06 +0800

    Initial Commit

---
+++ ColourMixer/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ColourMixer
+//
+//  Created by Jerry Tan on 22/02/2019.
+//  Copyright © 2019 Initiate Design®. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var redValue = 255
+    var greenValue = 255
+    var blueValue = 255
+
+    @IBOutlet weak var redText: UILabel!
+    @IBOutlet weak var greenText: UILabel!
+    @IBOutlet weak var blueText: UILabel!
+    
+    
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValue = Int(sender.value)
+        redText.text = "R: \(redValue)"
+        changeBackgroundColor()
+    }
+    
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = Int(sender.value)
+        greenText.text = "R: \(greenValue)"
+        changeBackgroundColor()
+    }
+    
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValue = Int(sender.value)
+        blueText.text = "R: \(redValue)"
+        changeBackgroundColor()
+    }
+    
+    
+    func changeBackgroundColor() {
+      view.backgroundColor =  UIColor(red: CGFloat(redValue) / 255, green: CGFloat(greenValue) / 255, blue: CGFloat(blueValue) / 255, alpha: 1)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
